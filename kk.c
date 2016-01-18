28db866aa13c4d861f711d6ff72437661e0e5080
Michele Orrù <maker@tumbolandia.net>
2016-01-18 01:02:22 +0000

    Use the heap, they said.

--- kk.c
+++ kk.c
@@ -38,13 +38,13 @@ void kk_sender(int sockfd, size_t m)
   }
 
   baseot_receiver(sockfd, CODEN, unpacked_delta, p[1]);
-  uint8_t Q[CODEN][ms/8];
+  uint8_t (*Q)[ms/8] = malloc(CODEN * sizeof(*Q));
   for (size_t i = 0; i < CODEN; ++i) {
     reading(p[0], Q[i], KAPPA/8);
     prg_extend(Q[i], ms/8);
   }
 
-  uint8_t u[ms/8];
+  uint8_t *u = malloc(ms/8 * sizeof(*u));
   for (size_t i = 0; i < CODEN; ++i) {
     reading(sockfd, u, ms/8);
     if (unpacked_delta[i]) {
@@ -52,7 +52,7 @@ void kk_sender(int sockfd, size_t m)
     }
   }
 
-  uint8_t QT[ms][CODEN/8];
+  uint8_t (*QT)[CODEN/8] = malloc(ms * sizeof(*QT));
   transpose(QT, Q, CODEN, ms);
   uint8_t q[CODEN/8];
   for (size_t j = 0; j < ms; ++j) {
@@ -61,11 +61,14 @@ void kk_sender(int sockfd, size_t m)
       bitand(q, codewords[i], CODEN);
       bitxor(q, QT[j], CODEN);
       hash(q, q, j, CODEN/8, KAPPA/8);
-      Bprint(q, KAPPA/8);
-      printf("\t");
+      // Bprint(q, KAPPA/8);
+      // printf("\t");
     }
-    printf("\n");
+    // printf("\n");
   }
+  free(Q);
+  free(QT);
+  free(u);
 }
 
 void kk_receiver(int sockfd, size_t m) {
@@ -80,8 +83,8 @@ void kk_receiver(int sockfd, size_t m) {
 
   /* note: rows here are columns in the paper. */
   baseot_sender(sockfd, CODEN, p[1]);
-  uint8_t T0[CODEN][ms/8];
-  uint8_t T1[CODEN][ms/8];
+  uint8_t (*T0)[ms/8] = malloc(CODEN * sizeof(*T0));
+  uint8_t (*T1)[ms/8] = malloc(CODEN * sizeof(*T0));
   for (int i = 0; i < CODEN; i++) {
     reading(p[0], T0[i], KAPPA/8);
     prg_extend(T0[i], ms/8);
@@ -89,17 +92,17 @@ void kk_receiver(int sockfd, size_t m) {
     prg_extend(T1[i], ms/8);
   }
 
-  uint8_t C[ms][CODEN/8];
-  uint8_t choices[ms];
+  uint8_t (*C)[CODEN/8] = malloc(ms * sizeof(*C));
+  uint8_t *choices = malloc(ms * sizeof(*choices));
   randombytes(choices, ms);
   for (size_t i = 0; i < ms; ++i) {
     choices[i] &= 1;
     memcpy(C[i], codewords + choices[i], sizeof(*codewords));
   }
 
-  uint8_t CT[CODEN][ms/8];
+  uint8_t (*CT)[ms/8] = malloc(CODEN * sizeof(*CT));
   transpose(CT, C, ms, CODEN);
-  uint8_t u[ms/8];
+  uint8_t *u = malloc(ms/8 * sizeof(*choices));
   for (size_t i = 0; i < CODEN; ++i) {
     memcpy(u, CT[i], ms/8);
     bitxor(u, T0[i], ms);
@@ -107,12 +110,20 @@ void kk_receiver(int sockfd, size_t m) {
     writing(sockfd, u, ms / 8);
   }
 
-  uint8_t T[ms][CODEN/8];
+  uint8_t (*T)[CODEN/8] = malloc(ms * sizeof(*T));
   uint8_t pad[KAPPA/8];
   transpose(T, T0, CODEN, ms);
   for (size_t j = 0; j < ms; j++) {
     hash(pad, T[j], j, CODEN/8, KAPPA/8);
-    Bprint(pad, KAPPA/8);
-    printf("\n");
+    // Bprint(pad, KAPPA/8);
+    // printf("\n");
   }
+
+  free(T0);
+  free(T1);
+  free(C);
+  free(choices);
+  free(CT);
+  free(u);
+  free(T);
 }
