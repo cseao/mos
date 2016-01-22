5097b093bf7d36a498649fb80b9b7fa6e1008c48
Michele Orrù <maker@tumbolandia.net>
2016-01-22 15:14:14 +0000

    Set runs to 50.

--- bench.sh
+++ bench.sh
@@ -3,7 +3,7 @@ flag=$1
 
 f() {
     times=$1
-    yes ./ot both $flag -m $times | head -n 100 | bash | tee ot-${times}${flag}.log
+    yes ./ot both $flag -m $times | head -n 50 | bash | tee ot-${times}${flag}.log
 }
 
 f 100000
