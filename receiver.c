4971de2a7979eb7d080d8d391efdce0a220b4aab
Michele Orrù <maker@tumbolandia.net>
2016-08-11 14:44:18 +0200

    Make other matrices in receiver.c type bitmatrix_t.

--- receiver.c
+++ receiver.c
@@ -16,8 +16,8 @@ static void
 receiver_check(const int sockfd, const size_t m)
 {
   bitmatrix_t mu = new_bitmatrix(SSEC, m);
-  uint8_t w[SSEC];
   bitmatrix_t t = new_bitmatrix(SSEC, CODEN);
+  uint8_t w[SSEC];
 
   /* Read μs from the network */
   for (int i = 0; i < SSEC; ++i) {
@@ -59,38 +59,38 @@ void kk_receiver(int sockfd, size_t m) {
 
   /* note: rows here are columns in the paper. */
   baseot_sender(sockfd, CODEN, p[1]);
-  uint8_t (*T0)[octs(ms)] = malloc(CODEN * sizeof(*T0));
-  uint8_t (*T1)[octs(ms)] = malloc(CODEN * sizeof(*T0));
+  bitmatrix_t T0 = new_bitmatrix(CODEN, ms);
+  bitmatrix_t T1 = new_bitmatrix(CODEN, ms);
   for (size_t i = 0; i < CODEN; i++) {
-    reading(p[0], T0[i], octs(KAPPA));
-    prgbits(T0[i], ms);
-    reading(p[0], T1[i], octs(KAPPA));
-    prgbits(T1[i], ms);
+    reading(p[0], row(T0, i), octs(KAPPA));
+    prgbits(row(T0, i), ms);
+    reading(p[0], row(T1, i), octs(KAPPA));
+    prgbits(row(T1, i), ms);
   }
 
-  uint8_t (*C)[octs(CODEN)] = malloc(ms * sizeof(*C));
+  bitmatrix_t C = new_bitmatrix(ms, CODEN);
   choices = malloc(ms * sizeof(*choices));
   randombits(choices, ms * 8);
   for (size_t i = 0; i < ms; ++i) {
     choices[i] &= codewordsm;
-    memcpy(C[i], codewords + choices[i], sizeof(*codewords));
+    bitcpy(row(C, i), codewords + choices[i], CODEN);
   }
 
-  uint8_t (*CT)[octs(ms)] = malloc(CODEN * sizeof(*CT));
-  transpose(CT, C, ms, CODEN);
-  free(C);
-  uint8_t *u = malloc(octs(ms) * sizeof(*u));
+  bitmatrix_t CT = new_bitmatrix(CODEN, ms);
+  transpose(CT.M, C.M, ms, CODEN);
+  free_bitmatrix(C);
+  uint8_t *u = bitalloc(ms);
   for (size_t i = 0; i < CODEN; ++i) {
-    bitcpy(u, CT[i], ms);
-    bitxor(u, T0[i], ms);
-    bitxor(u, T1[i], ms);
+    bitcpy(u, row(CT, i), ms);
+    bitxor(u, row(T0, i), ms);
+    bitxor(u, row(T1, i), ms);
     writebits(sockfd, u, ms);
   }
 
   T = new_bitmatrix(ms, CODEN);
-  transpose(T.M, T0, CODEN, ms);
-  free(T0);
-  free(T1);
+  transpose(T.M, T0.M, CODEN, ms);
+  free_bitmatrix(T0);
+  free_bitmatrix(T1);
 
   if (active_security) {
     receiver_check(sockfd, m);
@@ -106,7 +106,7 @@ void kk_receiver(int sockfd, size_t m) {
   }
 
   free(choices);
-  free(CT);
   free(u);
+  free_bitmatrix(CT);
   free_bitmatrix(T);
 }
