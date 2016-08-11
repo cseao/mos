1ae61fc319cb7467055149bbf742037bcdd39b74
Michele Orrù <maker@tumbolandia.net>
2016-08-11 09:43:14 +0200

    s/\/8/octs(·)/

--- receiver.c
+++ receiver.c
@@ -12,18 +12,18 @@
 
 
 static void
-receiver_check(const int sockfd,  uint8_t *choices, uint8_t (*T)[CODEN/8], const size_t m)
+receiver_check(const int sockfd,  uint8_t *choices, uint8_t (*T)[octs(CODEN)], const size_t m)
 {
-  uint8_t (*mu)[m/8] = malloc(SSEC * sizeof(*mu));
+  uint8_t (*mu)[octs(m)] = malloc(SSEC * sizeof(*mu));
   uint8_t w[SSEC];
-  uint8_t t[SSEC][CODEN/8];
+  uint8_t t[SSEC][octs(CODEN)];
 
   /* Read μs from the network */
   for (int i = 0; i < SSEC; ++i) {
-    reading(sockfd, mu[i], KAPPA/8);
-    prg_extend(mu[i], m/8);
+    reading(sockfd, mu[i], octs(KAPPA));
+    prg_extend(mu[i], octs(m));
     /* Initialize checks to base otp */
-    memcpy(t[i], T[m + i], CODEN/8);
+    memcpy(t[i], T[m + i], octs(CODEN));
     w[i] = choices[m + i];
   }
 
@@ -39,7 +39,7 @@ receiver_check(const int sockfd,  uint8_t *choices, uint8_t (*T)[CODEN/8], const
 
   /* Send check values */
   for (int i = 0; i < SSEC; ++i) {
-    writing(sockfd, t[i], CODEN/8);
+    writing(sockfd, t[i], octs(CODEN));
     writing(sockfd, &w[i], 1);
   }
   free(mu);
@@ -57,16 +57,16 @@ void kk_receiver(int sockfd, size_t m) {
 
   /* note: rows here are columns in the paper. */
   baseot_sender(sockfd, CODEN, p[1]);
-  uint8_t (*T0)[ms/8] = malloc(CODEN * sizeof(*T0));
-  uint8_t (*T1)[ms/8] = malloc(CODEN * sizeof(*T0));
+  uint8_t (*T0)[octs(ms)] = malloc(CODEN * sizeof(*T0));
+  uint8_t (*T1)[octs(ms)] = malloc(CODEN * sizeof(*T0));
   for (size_t i = 0; i < CODEN; i++) {
-    reading(p[0], T0[i], KAPPA/8);
-    prg_extend(T0[i], ms/8);
-    reading(p[0], T1[i], KAPPA/8);
-    prg_extend(T1[i], ms/8);
+    reading(p[0], T0[i], octs(KAPPA));
+    prg_extend(T0[i], octs(ms));
+    reading(p[0], T1[i], octs(KAPPA));
+    prg_extend(T1[i], octs(ms));
   }
 
-  uint8_t (*C)[CODEN/8] = malloc(ms * sizeof(*C));
+  uint8_t (*C)[octs(CODEN)] = malloc(ms * sizeof(*C));
   uint8_t *choices = malloc(ms * sizeof(*choices));
   randombytes(choices, ms);
   for (size_t i = 0; i < ms; ++i) {
@@ -74,18 +74,18 @@ void kk_receiver(int sockfd, size_t m) {
     memcpy(C[i], codewords + choices[i], sizeof(*codewords));
   }
 
-  uint8_t (*CT)[ms/8] = malloc(CODEN * sizeof(*CT));
+  uint8_t (*CT)[octs(ms)] = malloc(CODEN * sizeof(*CT));
   transpose(CT, C, ms, CODEN);
   free(C);
-  uint8_t *u = malloc(ms/8 * sizeof(*u));
+  uint8_t *u = malloc(octs(ms) * sizeof(*u));
   for (size_t i = 0; i < CODEN; ++i) {
-    memcpy(u, CT[i], ms/8);
+    memcpy(u, CT[i], octs(ms));
     bitxor(u, T0[i], ms);
     bitxor(u, T1[i], ms);
-    writing(sockfd, u, ms / 8);
+    writing(sockfd, u, octs(ms));
   }
 
-  uint8_t (*T)[CODEN/8] = malloc(ms * sizeof(*T));
+  uint8_t (*T)[octs(CODEN)] = malloc(ms * sizeof(*T));
   transpose(T, T0, CODEN, ms);
   free(T0);
   free(T1);
@@ -94,11 +94,11 @@ void kk_receiver(int sockfd, size_t m) {
     receiver_check(sockfd, choices, T, m);
   }
 
-  uint8_t pad[KAPPA/8];
+  uint8_t pad[octs(KAPPA)];
   for (size_t j = 0; j < m; ++j) {
-    hash(pad, T[j], j, CODEN/8, KAPPA/8);
+    hash(pad, T[j], j, octs(CODEN), octs(KAPPA));
 #ifndef NDEBUG
-    Bprint(pad, KAPPA/8);
+    Bprint(pad, octs(KAPPA));
     printf("\n");
 #endif
   }
--- sender.c
+++ sender.c
@@ -10,19 +10,19 @@
 #include "libot/ot.h"
 
 static void
-sender_check(const int sockfd, uint8_t delta[CODEN/8], uint8_t (*QT)[CODEN/8], const size_t m)
+sender_check(const int sockfd, uint8_t delta[octs(CODEN)], uint8_t (*QT)[octs(CODEN)], const size_t m)
 {
-  uint8_t (*mu)[m/8] = malloc(SSEC * sizeof(*mu));
-  uint8_t q[SSEC][CODEN/8];
-  uint8_t t[SSEC][CODEN/8];
-  uint8_t c[SSEC][CODEN/8];
+  uint8_t (*mu)[octs(m)] = malloc(SSEC * sizeof(*mu));
+  uint8_t q[SSEC][octs(CODEN)];
+  uint8_t t[SSEC][octs(CODEN)];
+  uint8_t c[SSEC][octs(CODEN)];
   uint8_t w[SSEC];
   for (int i = 0; i < SSEC; ++i) {
-    randombytes(mu[i], KAPPA/8);
-    writing(sockfd, mu[i], KAPPA/8);
-    prg_extend(mu[i], m/8);
+    randombytes(mu[i], octs(KAPPA));
+    writing(sockfd, mu[i], octs(KAPPA));
+    prg_extend(mu[i], octs(m));
 
-    memcpy(q[i], QT[m + i], CODEN/8);
+    memcpy(q[i], QT[m + i], octs(CODEN));
   }
 
   for (size_t j = 0; j < m; ++j) {
@@ -34,13 +34,13 @@ sender_check(const int sockfd, uint8_t delta[CODEN/8], uint8_t (*QT)[CODEN/8], c
   }
 
   for (int i = 0; i < SSEC; ++i) {
-    reading(sockfd, t[i], CODEN/8);
+    reading(sockfd, t[i], octs(CODEN));
     reading(sockfd, &w[i], 1);
     if (w[i] & ~codewordsm) {
       fprintf(stderr, "Check Failed!\n");
       exit(EXIT_FAILURE);
     }
-    memcpy(c[i], codewords[w[i]], CODEN/8);
+    memcpy(c[i], codewords[w[i]], octs(CODEN));
     bitxor(q[i], t[i], CODEN);
     bitand(c[i], delta, CODEN);
 
@@ -63,29 +63,29 @@ void kk_sender(int sockfd, size_t m)
   size_t ms = active_security? m + SSEC : m;
 
   /* Perform the base OTs, extends them and place those in a matrix Q. */
-  uint8_t delta[CODEN/8];
+  uint8_t delta[octs(CODEN)];
   uint8_t unpacked_delta[CODEN];
-  randombytes(delta, CODEN/8);
+  randombytes(delta, octs(CODEN));
   for (size_t i = 0; i < CODEN; ++i) {
     unpacked_delta[i] = getbit(delta, i);
   }
 
   baseot_receiver(sockfd, CODEN, unpacked_delta, p[1]);
-  uint8_t (*Q)[ms/8] = malloc(CODEN * sizeof(*Q));
+  uint8_t (*Q)[octs(ms)] = malloc(CODEN * sizeof(*Q));
   for (size_t i = 0; i < CODEN; ++i) {
-    reading(p[0], Q[i], KAPPA/8);
-    prg_extend(Q[i], ms/8);
+    reading(p[0], Q[i], octs(KAPPA));
+    prg_extend(Q[i], octs(ms));
   }
 
-  uint8_t *u = malloc(ms/8 * sizeof(*u));
+  uint8_t *u = malloc(octs(ms) * sizeof(*u));
   for (size_t i = 0; i < CODEN; ++i) {
-    reading(sockfd, u, ms/8);
+    reading(sockfd, u, octs(ms));
     if (unpacked_delta[i]) {
       bitxor(Q[i], u, ms);
     }
   }
 
-  uint8_t (*QT)[CODEN/8] = malloc(ms * sizeof(*QT));
+  uint8_t (*QT)[octs(CODEN)] = malloc(ms * sizeof(*QT));
   transpose(QT, Q, CODEN, ms);
 
   if (active_security) {
@@ -93,17 +93,17 @@ void kk_sender(int sockfd, size_t m)
   }
 
   uint8_t c[octs(CODEN)];
-  struct {uint8_t q[CODEN/8]; size_t j; } q_j;
+  struct {uint8_t q[octs(CODEN)]; size_t j; } q_j;
   for (q_j.j = 0; q_j.j < m; ++q_j.j) {
     for (size_t i = 0; i < codewordsn; i++) {
-      memcpy(q_j.q, delta, CODEN/8);
+      memcpy(q_j.q, delta, octs(CODEN));
       // XXX. also here intrinsics fucks up
       bitcpy(c, codewords[i], CODEN);
       bitand(q_j.q, c, CODEN);
       bitxor(q_j.q, QT[q_j.j], CODEN);
       base_hash((void *) &q_j, sizeof(q_j));
 #ifndef NDEBUG
-      Bprint(q_j.q, KAPPA/8);
+      Bprint(q_j.q, octs(KAPPA));
       printf("\t");
 #endif
     }
